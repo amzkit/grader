e888296bdd375a70906d6bd58e83d296fe44b433
Jatupat Klabklai <clan.sf123@gmail.com>
2022-04-13 15:36:18 +0700

    seed db user admin

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Database\Seeder;
+use App\Models\User;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +16,23 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call('UserTableSeeder');
+
+        $this->command->info('User table seeded!');
+    }
+}
+class UserTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('users')->delete();
+        User::create([
+            'name' => "Admin",
+            'email' => "admin_grader@mju.ac.th",
+            'password' => Hash::make('12345678'),
+            'role_admin' => 1,
+            'role' => 'admin',
+        ]);
     }
 }
